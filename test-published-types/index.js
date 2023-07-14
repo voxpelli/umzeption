f95df110a73bd72b1db863205b13d04977e9f222
Pelle Wessman <pelle@kodfabrik.se>
2023-07-14 17:29:43 +0200

    Fix published types test

--- test-published-types/index.js
+++ test-published-types/index.js
@@ -1,6 +1,21 @@
 /// <reference types="node" />
 
-import { something } from '@voxpelli/node-module-template';
+import { umzeption } from '@voxpelli/node-module-template';
+import { filename } from 'desm';
+import { Umzug, memoryStorage } from 'umzug';
 
-// eslint-disable-next-line no-console
-console.log(something());
+export async function doIt () {
+  const umzug = new Umzug({
+    migrations: umzeption({
+      dependencies: ['./fixtures/test-dependency'],
+      filePath: filename(import.meta.url),
+      glob: ['fixtures/migrations/*.js'],
+      name: 'main',
+    }),
+    context: {},
+    storage: memoryStorage(),
+    logger: console,
+  });
+
+  await umzug.up();
+}
